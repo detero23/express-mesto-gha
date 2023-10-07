175a76f708ba2d939d78deef7a5617d5f83b176d
Dmitry Konavalenak <konavalenak@mail.ru>
2023-10-08 00:41:28 +0300

    errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,11 +1,34 @@
 const Card = require("../models/card");
+const ERR_IncorrectData = {
+  code: 400,
+  name: "IncoorectDataError",
+  message: "Переданы некорректные данные",
+};
+const ERR_NotFound = {
+  code: 404,
+  name: "NotFoundError",
+  message: "Указанное id не найдено",
+};
+const ERR_Unknown = {
+  code: 500,
+  name: "UnknownError",
+  message: "Неизвестная ошибка",
+};
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch((err) =>
-      console.log(`Get cards error '${err.name}' - '${err.message}'`)
-    );
+    .catch((err) => {
+      if (err.name == "ValidationError") {
+        res.status(ERR_IncorrectData.code).send({
+          message: `${ERR_IncorrectData.message} при получении списка карточек`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка получения списка карточек '${err.name}' - '${err.message}'`,
+      });
+    });
 };
 
 module.exports.createCard = (req, res) => {
@@ -15,16 +38,37 @@ module.exports.createCard = (req, res) => {
   Card.create({ name, link, owner })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      console.log(`Create card error '${err.name}' - '${err.message}'`);
+      if (err.name == "ValidationError") {
+        res.status(ERR_IncorrectData.code).send({
+          message: `${ERR_IncorrectData.message} при создании карточки`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка создания карточки '${err.name}' - '${err.message}'`,
+      });
     });
 };
 
 module.exports.deleteCardById = (req, res) => {
   Card.findByIdAndRemove({ _id: req.params.id })
-    .then((card) => res.send({ data: card }))
-    .catch((err) =>
-      console.log(`Delete card by ID error '${err.name}' - '${err.message}'`)
-    );
+    .then((card) => {
+      if (!card) {
+        throw ERR_NotFound;
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name == "NotFoundError") {
+        res.status(ERR_NotFound.code).send({
+          message: `${ERR_NotFound.message} при удалении карточки`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка удаления карточки '${err.name}' - '${err.message}'`,
+      });
+    });
 };
 
 module.exports.putCardLike = (req, res) =>
@@ -33,10 +77,29 @@ module.exports.putCardLike = (req, res) =>
     { $addToSet: { likes: req.user._id } },
     { new: true }
   )
-    .then((card) => res.send({ data: card }))
-    .catch((err) =>
-      console.log(`Put card like error '${err.name}' - '${err.message}'`)
-    );
+    .then((card) => {
+      if (!card) {
+        throw ERR_NotFound;
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name == "NotFoundError") {
+        res.status(ERR_NotFound.code).send({
+          message: `${ERR_NotFound.message} при лайке карточки`,
+        });
+        return;
+      }
+      if (err.name == "ValidationError") {
+        res.status(ERR_IncorrectData.code).send({
+          message: `${ERR_IncorrectData.message} при лайке карточки`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка лайка карточки '${err.name}' - '${err.message}'`,
+      });
+    });
 
 module.exports.deleteCardLike = (req, res) =>
   Card.findByIdAndUpdate(
@@ -44,7 +107,26 @@ module.exports.deleteCardLike = (req, res) =>
     { $pull: { likes: req.user._id } },
     { new: true }
   )
-    .then((card) => res.send({ data: card }))
-    .catch((err) =>
-      console.log(`Delete card like error '${err.name}' - '${err.message}'`)
-    );
+    .then((card) => {
+      if (!card) {
+        throw ERR_NotFound;
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name == "NotFoundError") {
+        res.status(ERR_NotFound.code).send({
+          message: `${ERR_NotFound.message} при удалении лайка карточки`,
+        });
+        return;
+      }
+      if (err.name == "ValidationError") {
+        res.status(ERR_IncorrectData.code).send({
+          message: `${ERR_IncorrectData.message} при удалении лайка карточки`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка удаления лайка карточки '${err.name}' - '${err.message}'`,
+      });
+    });
--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,34 @@
 const User = require("../models/user");
-
-module.exports.getUserById = (req, res) => {
-  User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch((err) =>
-      console.log(`Get User by ID error '${err.name}' - '${err.message}'`)
-    );
+const ERR_IncorrectData = {
+  code: 400,
+  name: "IncoorectDataError",
+  message: "Переданы некорректные данные",
+};
+const ERR_NotFound = {
+  code: 404,
+  name: "NotFoundError",
+  message: "Указанное _id не найдено",
+};
+const ERR_Unknown = {
+  code: 500,
+  name: "UnknownError",
+  message: "Неизвестная ошибка",
 };
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) =>
-      console.log(`Get users error '${err.name}' - '${err.message}'`)
-    );
+    .catch((err) => {
+      if (err.name == "ValidationError") {
+        res.status(ERR_IncorrectData.code).send({
+          message: `${ERR_IncorrectData.message} при получении списка пользователей`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка получения списка пользователей '${err.name}' - '${err.message}'`,
+      });
+    });
 };
 
 module.exports.createUser = (req, res) => {
@@ -22,7 +37,36 @@ module.exports.createUser = (req, res) => {
   User.create({ name, about, avatar })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      console.log(`Create user error '${err.name}' - '${err.message}'`);
+      if (err.name == "ValidationError") {
+        res.status(ERR_IncorrectData.code).send({
+          message: `${ERR_IncorrectData.message} при создании пользователя`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка создания пользователя '${err.name}' - '${err.message}'`,
+      });
+    });
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        throw ERR_NotFound;
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name == "NotFoundError") {
+        res.status(ERR_NotFound.code).send({
+          message: `${ERR_NotFound.message} при получении пользователя`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка создания пользователя '${err.name}' - '${err.message}'`,
+      });
     });
 };
 
@@ -38,12 +82,29 @@ module.exports.updateUser = (req, res) => {
       upsert: false,
     }
   )
-    .then((user) => res.send({ data: user }))
-    .catch((err) =>
-      res
-        .status(500)
-        .send({ message: `Update user error '${err.name}' - '${err.message}'` })
-    );
+    .then((user) => {
+      if (!user) {
+        throw ERR_NotFound;
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name == "NotFoundError") {
+        res.status(ERR_NotFound.code).send({
+          message: `${ERR_NotFound.message} при обновлении пользователя`,
+        });
+        return;
+      }
+      if (err.name == "ValidationError") {
+        res.status(ERR_IncorrectData.code).send({
+          message: `${ERR_IncorrectData.message} при обновлении пользователя`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка обновления пользователя '${err.name}' - '${err.message}'`,
+      });
+    });
 };
 
 module.exports.updateAvatar = (req, res) => {
@@ -58,10 +119,27 @@ module.exports.updateAvatar = (req, res) => {
       upsert: false,
     }
   )
-    .then((user) => res.send({ data: user }))
-    .catch((err) =>
-      res.status(500).send({
-        message: `Update avatar error '${err.name}' - '${err.message}'`,
-      })
-    );
+    .then((user) => {
+      if (!user) {
+        throw ERR_NotFound;
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => {
+      if (err.name == "NotFoundError") {
+        res.status(ERR_NotFound.code).send({
+          message: `${ERR_NotFound.message} при обновлении аватара`,
+        });
+        return;
+      }
+      if (err.name == "ValidationError") {
+        res.status(ERR_IncorrectData.code).send({
+          message: `${ERR_IncorrectData.message} при обновлении аватара`,
+        });
+        return;
+      }
+      res.status(ERR_Unknown.code).send({
+        message: `Ошибка обновления аватара '${err.name}' - '${err.message}'`,
+      });
+    });
 };
