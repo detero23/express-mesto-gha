a1a8bc207e3469e99a1996d1169844fc119a88ad
Dmitry Konavalenak <konavalenak@mail.ru>
2023-10-19 00:54:02 +0300

    auth fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,8 +6,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .next(new NoAuthError('Требуется авторизация'));
+    return next(new NoAuthError('Требуется авторизация'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,8 +15,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, 'My_Custom_Secret');
   } catch (err) {
-    return res
-      .next(new NoAuthError('Требуется авторизация'));
+    return next(new NoAuthError('Требуется авторизация'));
   }
 
   req.user = payload;
